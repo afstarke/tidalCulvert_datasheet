412f303a6dc103972e19792838d7a064f01259be
Adam Starke <astarke@tnc.org>
2019-08-02 18:08:14 -0400

    attempt at fixing bugs in height extraction code.

--- functions/culvert_tidy.R
+++ functions/culvert_tidy.R
@@ -1,6 +1,5 @@
 # Culvert tidy
 # Read in tidy style.
-# using functional programming
 
 # Read in files from specified folder
 # Create tibble with columns of spreadsheet name, size, last modified, and list column with tidyxl 'cells' 
@@ -27,14 +26,46 @@ culvert_fetch <- function(filepath){
   return(cells)
 }
 
+# ------------------------------------------------------------------------ 
+# 
+#' surveyHtCorrection
+#'
+#' @param rawHeight height as recorded in field.
+#' @param shotCode control point code, R, U, D, X
+#' @param Lidarht 
+#' @param roadCentHt 
+#' @param TPforsight_upSt 
+#' @param TPbacksight_upSt 
+#' @param TPforsight_dwSt 
+#' @param TPbacksight_dwSt 
+#'
+#' @return numeric value of height corrected to NAVD88
+#' @export
+#'
+#' @examples
+#' 
+surveyHtCorrection <- function(rawHeight, shotCode, Lidarht, roadCentHt, TPforsight_upSt, TPbacksight_upSt, TPforsight_dwSt, TPbacksight_dwSt){
+  case_when(shotCode == "R" ~ (Lidarht + roadCentHt - rawHeight),
+            shotCode == "U" ~ (Lidarht + roadCentHt - TPforsight_upSt + TPbacksight_upSt - rawHeight),
+            shotCode == "D" ~ (Lidarht + roadCentHt - TPforsight_dwSt + TPbacksight_dwSt - rawHeight),
+            shotCode == "X" ~ -9999, # missing data value for the 'X' code that I don't know how to deal with yet.
+            is.na(shotCode) ~ -9999) # if missing shot code, it's likely missing other data.....
+}
+
+blankCheck <- function(df){
+  if(dim(df)[1] == 0){
+    NA
+  }else{
+    df %>% pull() %>% as.numeric()
+  }
+}
 
 # ------------------------------------------------------------------------
-# TODO: The plan for this function is to more or less replicate the larger culvert_extract function set but only targeting the cross section data to simplfy the process. 
-
 #' ---- channelLongidinalProfile_extract
 #' Fetch longitudinal profile from a tidal crossing Assessment Workbook 
 #' @param filepath A path to excel file containing Tidal Culvert datasheet.
-#' @return tidy dataframe containing information relating to the cross sectional profile measured in field and recorded in M99:M102 and A122:M140
+#' @return tidy dataframe containing information relating to the cross sectional
+#'   profile measured in field and recorded in M99:M102 and A122:M140
 #'  
 #'
 #' @examples
@@ -43,9 +74,25 @@ culvert_fetch <- function(filepath){
 # filepath <- list.files(tidalCulvert_datasheetsFolder, full.names = T)[[3]] 
 
 channelLongidinalProfile_extract <- function(filepath){
-  profile <- read.xlsx(filepath, sheetIndex = 2, rowIndex = 122:140, colIndex = 1:13)
-  profile <- profile %>% select(Distance, Height, Feature.Code, ShotFrom = Shot.From..R.U.D.) %>% 
-    filter(!is.na(Distance))
+  # Set up variables for adjusting to NAVD88
+  Lidarht <- read_xlsx(path = filepath, sheet = 3, range = "J54", col_names = FALSE) %>% pull() %>% as.numeric()
+  roadCentHt <- read_xlsx(path = filepath, sheet = 2, range = "J107", col_names = FALSE) %>% pull() %>% as.numeric()
+  TPforsight_upSt <- read_xlsx(path = filepath, sheet = 2, range = "Y110", col_names = FALSE) %>% pull() %>% as.numeric() 
+  TPbacksight_upSt <- read_xlsx(path = filepath, sheet = 2, range = "Y111", col_names = FALSE) %>% pull() %>% as.numeric()
+  TPforsight_dwSt <- read_xlsx(path = filepath, sheet = 2, range = "AD110", col_names = FALSE) %>% pull() %>% as.numeric()
+  TPbacksight_dwSt <- read_xlsx(path = filepath, sheet = 2, range = "AD111", col_names = FALSE) %>% pull() %>% as.numeric()
+  
+  profile <- read_xlsx(path = filepath, sheet = 2, range = "A122:M140")
+  profile <- profile %>% filter(!is.na(Distance)) %>% 
+    select(-starts_with("..")) %>% 
+    rename(Subsrate = `Sub-\r\nstrate`,
+           shotCode = `Shot From (R/U/D)`,
+           rawHeight = Height) %>% 
+    mutate(adjustedHt = surveyHtCorrection(rawHeight = rawHeight, shotCode = shotCode, Lidarht = Lidarht, roadCentHt = roadCentHt, 
+                                           TPforsight_upSt = TPforsight_upSt, TPbacksight_upSt = TPbacksight_upSt, 
+                                           TPforsight_dwSt = TPforsight_dwSt, TPbacksight_dwSt = TPbacksight_dwSt))
+      
+  
   profile
 }
 
@@ -53,12 +100,26 @@ channelLongidinalProfile_extract <- function(filepath){
 # 
 
 crossSection <- function(filepath){
-  cross <- read.xlsx(filepath, sheetIndex = 2, rowIndex = 98:111, colIndex = 18:34)
-  cross <- cross %>% filter(!is.na(Feature), Feature != "IF NEEDED:") %>% 
-    select(1, 8, shotFromUS = 12, 13, shotFromDS = 17)
+  Lidarht <- read_xlsx(path = filepath, sheet = 3, range = "J54", col_names = FALSE) %>% pull() %>% as.numeric()
+  roadCentHt <- read_xlsx(path = filepath, sheet = 2, range = "J107", col_names = FALSE) %>% pull() %>% as.numeric()
+  TPforsight_upSt <- read_xlsx(path = filepath, sheet = 2, range = "Y110", col_names = FALSE) %>% pull() %>% as.numeric() 
+  TPbacksight_upSt <- read_xlsx(path = filepath, sheet = 2, range = "Y111", col_names = FALSE) %>% pull() %>% as.numeric()
+  TPforsight_dwSt <- read_xlsx(path = filepath, sheet = 2, range = "AD110", col_names = FALSE) %>% pull() %>% as.numeric()
+  TPbacksight_dwSt <- read_xlsx(path = filepath, sheet = 2, range = "AD111", col_names = FALSE) %>% pull() %>% as.numeric()
+  
+  cross <- read_xlsx(path = filepath, sheet = 2, range = "R98:AH106", trim_ws = TRUE)
+  cross <- cross %>% rename(cntrlPtcode_US = '..12', cntrlPtcode_DS = '..17') %>% 
+    select(-starts_with("..")) %>% 
+    mutate(adjustedHtUS = surveyHtCorrection(rawHeight = `US Height`, shotCode = cntrlPtcode_US, Lidarht = Lidarht, roadCentHt = roadCentHt, 
+                                           TPforsight_upSt = TPforsight_upSt, TPbacksight_upSt = TPbacksight_upSt, 
+                                           TPforsight_dwSt = TPforsight_dwSt, TPbacksight_dwSt = TPbacksight_dwSt),
+           adjustedHtDS = surveyHtCorrection(rawHeight = `DS Height`, shotCode = cntrlPtcode_DS, Lidarht = Lidarht, roadCentHt = roadCentHt, 
+                                             TPforsight_upSt = TPforsight_upSt, TPbacksight_upSt = TPbacksight_upSt, 
+                                             TPforsight_dwSt = TPforsight_dwSt, TPbacksight_dwSt = TPbacksight_dwSt))
   cross
-}
 
+  }
+# TODO: Add the height sections from the functions above into the data extraction culvert Tidy function below.
 # ---- culvert_tidy
 #' Create a tidy (nested)data frame of tidal culvert datasheets with 
 #' a column containing tidyxl dataframes for each cooresponding file.
@@ -81,8 +142,7 @@ culvert_tidy <- function(folder){
                                     origin = "1970-01-01"),
                                           filePath = paste(folder, filenames, sep = "/")) # create a value for last changes to file for QA. 
   culvertFiles <- culvertFiles %>% mutate(tidycells = map(.x = filePath, .f = culvert_fetch))#, # For each file in the table, 'culvert_fetch' the data.
-                                          # longProfile = map(.x = filePath, .f = possibly(channelLongidinalProfile_extract, otherwise = "HELP ME")),
-                                          # crossSectionalProfile = map(.x = filePath, .f = crossSection)) 
+                                          
   return(culvertFiles)
   
 }
