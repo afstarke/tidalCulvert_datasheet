7a0e27db0caa6b878a1efe67b02214c24473d3de
Adam Starke <astarke@tnc.org>
2020-04-28 14:14:33 -0400

    spring cleaning

--- pivotTTabl.R
+++
@@ -1,6 +0,0 @@
-# Pivot Table
-
-rpivotTable(LIculvertDataStatus_location, rows = c("ToBeAssessed_2019", "FieldAssessmentComplete"), 
-            cols = "DesktopAssessmentComplete", width = "100%",
-            rendererName = "Heatmap")
-
