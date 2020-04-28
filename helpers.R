5b8761cfffe930aa75a68df9c527e5163620bbbe
Adam Starke <astarke@tnc.org>
2020-04-28 11:23:08 -0400

    deletes and moves.

--- helpers.R
+++
@@ -1,7 +0,0 @@
-# Helper functions
-
-# Save a summaryRoutput dataframe to excel for others to review ----
-
-saveSumary <- function(summaryDF){
-  xlsx::write.xlsx(x = summaryDF, file = "outputs/summaryRoutput.xlsx", showNA = FALSE)
-}
