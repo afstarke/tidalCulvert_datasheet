1a4b1dcb3178318dcedce70e8c3d26f3c9a69f08
Adam Starke <astarke@tnc.org>
2020-05-08 09:33:25 -0400

    updates for remaking fw sheets

--- utilities/summarySheet_generator.R
+++ utilities/summarySheet_generator.R
@@ -8,14 +8,16 @@ source(here::here("summarySheetSetup.R"))
 crossing_list <- fw_data %>% filter(Total_Benefit != -99) %>% pull(CrosCode)
 
 
-
+# Render Rmd to HTML.
 makeDocs <- function() {
   # Pandoc errors occaisonally throwing off loop.
   # Create a log of the pdf files that have been produced and rerun above loop removing the ones already created.
   # Successes are crossings that have been converted to pdf.
   successes <-
-    list.files("../../../Box Sync/LI_Road_Stream_Tidal_Crossing_Project_Resources/Documents/SummarySheets", pattern = ".pdf") %>% str_remove(pattern = ".pdf")
-    # list.files("D:/culvert_project/html_outputs", pattern = ".html") %>% str_remove(pattern = ".html")
+    # list.files("../../../Box Sync/LI_Road_Stream_Tidal_Crossing_Project_Resources/Documents/SummarySheets", pattern = ".pdf") %>% str_remove(pattern = ".pdf")
+    # list.files("D:/culvert_project/html_outputs", pattern = ".pdf") %>% strstr_remove(pattern = ".pdf")
+    # list.files("D:/culvert_project/html_outputs", pattern = ".html") %>% str_remove(pattern = ".html") # DElete bad copies of the htmls here to reproduce.
+    list.files("D:/culvert_project/pdf_outputs", pattern = ".pdf") %>% str_remove(pattern = ".pdf") # DElete bad copies of the htmls here to reproduce.
   print(paste(
     length(successes),
     "successfully printed, just",
@@ -28,7 +30,7 @@ makeDocs <- function() {
   purrr::walk(
     .x = todos,
     .f = ~ rmarkdown::render(
-      "summarySheets/summarySheet_fw_postersimpler.Rmd",
+      "summarySheets/summarySheet_fw.Rmd",
       poster_jacobs(css = "summary.css"),
       params = list(crossingCode = .x),
       output_file = paste0(.x, ".html"),
@@ -43,88 +45,6 @@ makeDocs()
 
 
 
-# Then loop through the html files and create them as pdfs.
-# OR---
-# Add knit: pagedown::chrome_print to yaml - which isn't working....
-
-# 
-# printDocs <- function() {
-#   # Create the list of files to convert
-#   files <-
-#     list.files(path = "D:/culvert_project/html_outputs/",
-#                pattern = ".html",
-#                full.names = F)
-#   # create a list of successes to pick up in case code breaks. These are pdfs
-#   successes <-
-#     list.files("D:/culvert_project/html_outputs/") %>% str_remove(pattern = ".pdf")
-#   filestoconvert <-
-#     files %>% str_remove(pattern = ".html") # vector of file names(crossingnames)
-#   
-#   todos <- filestoconvert[!filestoconvert %in% successes]
-#   
-#   print(paste(
-#     length(successes),
-#     "successfully printed, just",
-#     length(crossing_list) - length(successes),
-#     "more to go."
-#   ))
-#   # for (i in files) {
-#   #   chrome_print(
-#   #     verbose = 0,
-#   #     input = i,
-#   #     timeout = 30,
-#   #     output = here::here("summarySheets/pdf_outputs/"),
-#   #     wait = 3,
-#   #     # browser = "C:/Program Files (x86)/Google/Chrome Beta/Application/chrome.exe",
-#   #     extra_args = c("--disable-gpu", "--no-sandbox", "--headless"),
-#   #     options = list(margins = "none")
-#   #   )
-#   #
-#   # }
-#   
-#   purrr::walk(
-#     .x = todos,
-#     .f = ~ chrome_print(
-#       input = paste0("D:/culvert_project/html_outputs/", files),
-#       wait = 3,
-#       # work_dir = here::here("summarySheets/html_outputs/tmp"),
-#       timeout = 30,
-#       verbose = 1,
-#       # browser = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe",
-#       # extra_args = c("--disable-gpu", "--no-sandbox", "--headless"),
-#       options = list(margins = "none"),
-#       extra_args = c("--headless", "--no-sandbox", "--disable-gpu")
-#     )
-#   )
-#   
-# }
-# printDocs()
-# # 
-# pagedown::chrome_print(input = "D:/culvert_project/html_outputs/xy4066780473404765.html",
-#               wait = 10,
-#              timeout = 20,
-#              verbose = 2,
-#              extra_args = c("--no-sandbox"),
-#              browser = "C:/Program Files (x86)/Google/Chrome Beta/Application/chrome.exe",
-#              # extra_args = c("--disable-gpu", "--no-sandbox", "--headless"),
-#              options = list(margins = "none"))#,
-#              # extra_args = c("--headless", "--no-sandbox", "--disable-gpu"))
-# # # # 
-# # # pagedown options: 
-# chrome_print(input = "summarySheets/summarySheet_fw_postersimpler.Rmd",
-#                        output = "",
-#                        # format = "pdf",
-#                        # timeout = 30,
-#                        # verbose = 1,
-#                        wait = 5,
-#                        extra_args = c("--disable-gpu", "--no-sandbox", "--headless"),
-#                        browser = "C:/Program Files (x86)/Google/Chrome Beta/Application/chrome.exe")
-# 
-
-# 
-
-
-
 
 
 # Manually printing these things...
@@ -133,16 +53,16 @@ makeDocs()
  
 
 
-
-allhtmls <- list.files(path = "D:/culvert_project/html_outputs/",
+# list the HTMLs that are in the working output directory. These are considered done- 
+allhtmls <- list.files(path = "D:/culvert_project/html_outputs/", # Remove any files from this directory that are UNWANTED.
              pattern = ".html",
              full.names = F) %>% str_remove(".html")
-# create a list of successes to pick up in case code breaks. These are pdfs created from the htmls
+# create a list of successes to pick up in case code breaks. These are pdfs created from the htmls that are listed from above. 
 pdfcheck <- function()
 { # returns a pep talk and the list of crossings that are in the pdf output folder.
   pdfs_completed <-
     list.files(
-      "../../../Box Sync/LI_Road_Stream_Tidal_Crossing_Project_Resources/Documents/SummarySheets"
+      "D:/culvert_project/pdf_outputs/" # Similar to above remove pdf versions that are UNWANTED. i.e. page breaks wrong etc. 
     ) %>%
     str_remove(pattern = ".pdf")
   print(paste0(
